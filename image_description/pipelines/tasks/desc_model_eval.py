2ea0906fa0915d63de11d0969ee666639a0601b4
Kamatchi <85495544+Kamatchi-23@users.noreply.github.com>
2025-05-02 16:09:29 +1000

    added pipeline

--- image_description/pipelines/tasks/desc_model_eval.py
+++ image_description/pipelines/tasks/desc_model_eval.py
@@ -43,7 +43,7 @@ params = {
     'dataset_name': 'Desc_Caption_EvalDataset ',              # latest registered dataset
     'eval_dataset_id': '', #'e19da140dd6a479c864dd7bdf930918d',#'2231b5b121924ed684d6560cf6839619',     # specific version of the dataset
     'eval_dataset_name': 'eval_dataset_zip',
-    'desc_draft_model_id': '', #'36939d5f9c7a41a2b75ee2110e155144',       # the unpublished model to evaluate 
+    'desc_draft_model_id': '6d7b1be6082543efaade2fb24df63ae3', #'36939d5f9c7a41a2b75ee2110e155144',       # the unpublished model to evaluate 
     'desc_pub_model_name': 'student_desc_model',       # the published model name for comparison
 }
 task.connect(params)
@@ -127,8 +127,8 @@ if not test_json or not images_dir:
     task.mark_completed(status_message="No dataset provided for evaluation.")
     exit(0)
 # no model provided for evaluation
-#if not draft_model_id:
-    #raise ValueError("Missing new/draft model. Please provide draft_model_id.")
+if not draft_model_id:
+    raise ValueError("Missing new/draft model. Please provide draft_model_id.")
 # Mandatory input param
 if not pub_model_name:
     raise ValueError("Missing model. Please provide pub_model_name.")
