bb5afea2b2de2cf264ddc1d448a015b0989abdfd
Kamatchi <85495544+Kamatchi-23@users.noreply.github.com>
2025-05-21 15:04:00 +1000

    added files

--- image_description/pipelines/tasks/desc_hpo.py
+++ image_description/pipelines/tasks/desc_hpo.py
@@ -1,18 +1,18 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src')))
 from clearml import Task, Dataset
 from clearml.automation import HyperParameterOptimizer
-from clearml.automation import UniformIntegerParameterRange, UniformParameterRange
+from clearml.automation import DiscreteParameterRange, UniformIntegerParameterRange, UniformParameterRange
 import logging
 import time
 import json
 import yaml
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src')))
 from enigmaai.config import Project, ConfigFactory
 from enigmaai import util
 
 # get project configurations
-project = ConfigFactory.get_config(Project.HAZARD_DETECTION)
+project = ConfigFactory.get_config(Project.SCENE_DESCRIPTION)
 project_name = project.get('project-name')
 
 # Set up logging
@@ -21,19 +21,23 @@ logger = logging.getLogger(__name__)
 
 # Initialize the HPO task
 task = Task.init(project_name=project_name, 
-                task_name="Model HPO", 
+                task_name="step7_desc_model_hpo", 
                 task_type=Task.TaskTypes.optimizer,
                 reuse_last_task_id=False
 )
 
 params = {
-    'base_task_id': '', 
-    'hpo_min_batch': 2,             # minimum batch size in HPO range
-    'hpo_max_batch': 6,             # maximum batch size in HPO range
-    'hpo_min_weight_decay': 1e-5,   # minimum weight decay
-    'hpo_max_weight_decay': 1e-5,   # maximum weight decay
-    'total_max_jobs': 3,            # Total maximum job for the optimization process
-    'max_job_iter': 5,              # Number of iteration per job ‘iterations’ for the specified objective
+    'base_train_task_id': 'c82103e6f8ba4157ae94adc85aad8689', 
+    'run_as_service': False,
+    'test_queue': 'desc_preparation',  # Queue for test tasks
+    #'processed_dataset_id': '99e286d358754697a37ad75c279a6f0a',  # Will be set from pipeline
+    'num_epochs': [5, 10, 20],  # Reduced from 50 to 20 epochs
+    'batch_size': [16, 32],
+    #'batch_size_max': 32, 
+    'learning_rate': [6e-5, 8e-5, 1e-4, 2e-4],
+    #'learning_rate_max': 1e-4,
+    #'weight_decay_max': 1e-2,
+    'weight_decay': [1e-3, 5e-3, 1e-2],  # Default weight decay
 }
 
 task.connect(params)
@@ -41,69 +45,49 @@ task_params = task.get_parameters()
 task.execute_remotely(queue_name=project.get('queue-gpu'))
 logger.info(f"model_HPO params={task_params}")
 
-base_task_id = task_params['General/base_task_id']
+base_task_id = task_params['General/base_train_task_id']
 
 # Exit if not base task
 if not base_task_id:
-    task.mark_completed(status_message="No base task ID provided. Nothing to optimisation from.")
+    task.mark_completed(status_message="No base train task ID provided. Nothing to optimisation from.")
     exit(0)
 
 # Create the HPO task
 hpo_task = HyperParameterOptimizer(
     base_task_id=base_task_id,
     hyper_parameters=[
-        UniformIntegerParameterRange('General/batch', 
-                                     min_value=int(task_params['General/hpo_min_batch']), 
-                                     max_value=int(task_params['General/hpo_max_batch'])), 
-        UniformParameterRange('General/weight_decay', 
-                              min_value=float(task_params['General/hpo_min_weight_decay']), 
-                              max_value=float(task_params['General/hpo_max_weight_decay']))  
-    ],
+        DiscreteParameterRange('General/num_epochs', values=list(task_params['General/num_epochs'])),
+        DiscreteParameterRange('General/batch_size', values=list(task_params['General/batch_size'])), 
+        DiscreteParameterRange('General/learning_rate', values=list(task_params['General/learning_rate'])),  
+        DiscreteParameterRange('General/weight_decay', values=list(task_params['General/weight_decay']))],
     objective_metric_title='validation',
-    objective_metric_series='recall',
+    objective_metric_series='cider',
     objective_metric_sign='max',
-    max_number_of_concurrent_tasks=10,
-    total_max_jobs=int(task_params['General/total_max_jobs']),
+    max_number_of_concurrent_tasks=5,
     min_iteration_per_job=1,
-    max_iteration_per_job=int(task_params['General/max_job_iter']),
-    pool_period_min=10, 
     execution_queue=project.get('queue-gpu'),
-    save_top_k_tasks_only=1
-)
+    save_top_k_tasks_only=1)
 
 # Get the top performing experiments
-def get_top_task_exp(job_id, objective_value, objective_iteration, 
-                     job_parameters,top_performance_job_id):
-    
+def get_top_task_exp():
     best_task = hpo_task.get_top_experiments(top_k=1)[0] 
     logger.info(f"Best experiment: {best_task.id}")
-    
     # Get the best parameters and accuracy
     best_params = best_task.get_parameters()
     metrics = best_task.get_all_reported_scalars()
-    best_recall = metrics['validation']['recall'] if metrics and 'validation' in metrics and 'recall' in metrics['validation'] else None
-    
+    best_cider = metrics['validation']['cider'] if metrics and 'validation' in metrics and 'cider' in metrics['validation'] else None
     # Save best parameters and accuracy
     best_results = {
         'parameters': best_params,
-        'best_metrics': best_recall
-    }
-    
+        'best_metrics': best_cider}
     # Upload as artifact
     task.upload_artifact('best_parameters', best_results)
-    print("best results:", best_results)
-    
+    print("best results:", best_results)    
     # task output info
     best_model = best_task.models.output[0]
     task.set_parameter("best_model_project", project_name)
     task.set_parameter("best_model_task_id", best_model.name)
     task.set_parameter("best_model_id", best_model.id)
-    task.set_parameter("best_model_name", best_model.name)
-    task.set_parameter("best_model_variant", best_model.name)
-    
-# This will automatically create and print the optimizer new task id
-# for later use. if a Task was already created, it will use it.
-# hpo_task.set_time_limit(in_minutes=720.)
 
 # Start the HPO task
 logger.info("Starting HPO task...")
